80e47d009fc71db7e94bf0270bff0ce5cf54599b
Lev Tseytlin <lev@online-digitalx.de>
2021-08-25 10:49:47 +0200

    Initial Commit

---
+++ Shared/PlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundAppApp.swift
+//  Shared
+//
+//  Created by Lev Tseytlin on 25.08.21.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
